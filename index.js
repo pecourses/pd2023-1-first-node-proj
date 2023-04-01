b5ee4f6e9b52394f354848b087163c171bc899c2
Viktoriia <pecourses>
2023-04-01 13:42:54 +0300

    Add lodash

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello from node))) ')
+// import _ from 'lodash'
+const _ = require('lodash')
+
+const user = {
+  id: 1,
+  firstName: 'Test',
+  lastName: 'Testowich',
+  email: 'test@test.com',
+  passw: 'j35dg4dglsjdciof5'
+}
+
+const preparedUser = _.omit(user, ['passw'])
+
+console.log('preparedUser :>> ', preparedUser)
